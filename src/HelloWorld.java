88c7d41aaa62bc6209d403883a68abf28c8311f5
Sabina Civgin <cemalovicsabina@gmail.com>
2024-11-05 13:19:12 +0100

    Update HelloWorld.java
    Cats and dogs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello cat");
+        System.out.println("Hello cats");
         // skriva ut Hello World på skärm
-        System.out.println("Hello dog");
+        System.out.println("Hello dogs");
         System.out.println("Hello Sabina!");
     }
 }
