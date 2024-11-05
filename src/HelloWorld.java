16179e9a86058fa24118143c945854ef1a50c2e1
sabina-ec <cemalovicsabina@gmail.com>
2024-11-05 12:49:33 +0100

    Hello dog

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // skriva ut Hello World på skärm
-        System.out.println("Hello world");
+        System.out.println("Hello dog");
     }
 }
